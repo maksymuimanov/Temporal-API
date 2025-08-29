526151ec57858372bd9ca8a94ae2f525a9241dab
w4t3rcs <uimanovmaks@gmail.com>
2025-08-29 10:18:17 +0200

    Tine cleanup

--- neoforged-mod/src/main/java/com/temporal/api/core/engine/io/metadata/strategy/field/event/RegisterHangingSignRendererStrategy.java
+++ neoforged-mod/src/main/java/com/temporal/api/core/engine/io/metadata/strategy/field/event/RegisterHangingSignRendererStrategy.java
@@ -4,16 +4,16 @@ import com.temporal.api.core.engine.event.handler.EntityRendererRegisterRenderer
 import com.temporal.api.core.engine.io.metadata.annotation.event.RegisterHangingSignRenderer;
 import com.temporal.api.core.engine.io.metadata.strategy.field.FieldAnnotationStrategy;
 import net.minecraft.client.renderer.blockentity.HangingSignRenderer;
+import net.minecraft.core.Holder;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.HangingSignBlockEntity;
-import net.neoforged.neoforge.registries.DeferredHolder;
 
 import java.lang.reflect.Field;
 
 public class RegisterHangingSignRendererStrategy implements FieldAnnotationStrategy<RegisterHangingSignRenderer> {
     @Override
     public void execute(Field field, Object object, RegisterHangingSignRenderer annotation) throws Exception {
-        var hangingSignBlockEntity = (DeferredHolder<BlockEntityType<?>, BlockEntityType<HangingSignBlockEntity>>) field.get(object);
+        var hangingSignBlockEntity = (Holder<BlockEntityType<HangingSignBlockEntity>>) field.get(object);
         EntityRendererRegisterRendererEventHandler.RENDERING_REGISTRIES.add(event -> event.registerBlockEntityRenderer(hangingSignBlockEntity.value(), HangingSignRenderer::new));
     }
 
--- neoforged-mod/src/main/java/com/temporal/api/core/engine/io/metadata/strategy/field/event/RegisterSignRendererStrategy.java
+++ neoforged-mod/src/main/java/com/temporal/api/core/engine/io/metadata/strategy/field/event/RegisterSignRendererStrategy.java
@@ -4,16 +4,16 @@ import com.temporal.api.core.engine.event.handler.EntityRendererRegisterRenderer
 import com.temporal.api.core.engine.io.metadata.annotation.event.RegisterSignRenderer;
 import com.temporal.api.core.engine.io.metadata.strategy.field.FieldAnnotationStrategy;
 import net.minecraft.client.renderer.blockentity.SignRenderer;
+import net.minecraft.core.Holder;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
-import net.neoforged.neoforge.registries.DeferredHolder;
 
 import java.lang.reflect.Field;
 
 public class RegisterSignRendererStrategy implements FieldAnnotationStrategy<RegisterSignRenderer> {
     @Override
     public void execute(Field field, Object object, RegisterSignRenderer annotation) throws Exception {
-        var signBlockEntity = (DeferredHolder<BlockEntityType<?>, BlockEntityType<SignBlockEntity>>) field.get(object);
+        var signBlockEntity = (Holder<BlockEntityType<SignBlockEntity>>) field.get(object);
         EntityRendererRegisterRendererEventHandler.RENDERING_REGISTRIES.add(event -> event.registerBlockEntityRenderer(signBlockEntity.value(), SignRenderer::new));
     }
 
